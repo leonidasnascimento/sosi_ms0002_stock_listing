e8593eed06f0e7826720983692e6e676dec37b80
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-12-24 11:58:38 -0300

    Improvements on Redis Cache Server connection & operations

--- stock_listing/data/redis_manager.js
+++ stock_listing/data/redis_manager.js
@@ -3,70 +3,122 @@ const redis = require('redis')
 let redis_client = null
 let is_redis_key_exists = true
 let is_redis_server_on = true
+let redis_return_obj = {
+    message: "",
+    key_found: false,
+    server_on: false,
+    data: null
+}
 
 module.exports = class {
-    create_redis_client() {
-        redis_client = redis.createClient({
-            host: 'redis-15915.c56.east-us.azure.cloud.redislabs.com',
-            no_ready_check: true,
-            auth_pass: 'aJ2Yp42kM4GjG188uP65xFudaT3h6CyM',
-            port: 15915
-        });
-
-        redis_client.on('error', function (err) {
-            is_redis_server_on = false
-            console.warn('Server is not on: ' + err)
-        });
-    }
-
-    get_key_json_value(redis_key, on_success, on_error) {
+    create_redis_client(key, skip_key_checking = false) {
         try {
-            this.create_redis_client()
-
-            redis_client.exists(redis_key, function (err, reply) {
-                if (reply === 1) {
-                    is_redis_key_exists = true
-                } else {
-                    is_redis_key_exists = false
-                }
+            redis_client = redis.createClient({
+                host: 'redis-15915.c56.east-us.azure.cloud.redislabs.com',
+                no_ready_check: false,
+                auth_pass: 'aJ2Yp42kM4GjG188uP65xFudaT3h6CyM',
+                port: 15915
             });
 
-            if (is_redis_server_on && is_redis_key_exists) {
-                redis_client.get(redis_key, function (err, reply) {
-                    if (!(reply === undefined) && (reply !== "")) {
-                        on_success(reply)
-                        return
-                    } else {
-                        on_success(null)
-                    }
-                })
+            redis_client.on('error', function (err) {
+                redis_return_obj.server_on = false
+                redis_return_obj.message = "Error"
+                redis_return_obj.data = err
+            });
+
+            redis_client.on('connect', function (err) {
+                redis_return_obj.server_on = true
+            });
+
+            if (skip_key_checking) {
+                redis_return_obj.key_found = true
             } else {
-                on_success(null)
+                redis_client.exists(key, function (err, reply) {
+                    if (reply === 1) {
+                        redis_return_obj.key_found = true
+                    } else {
+                        redis_return_obj.key_found = false
+                    }
+                });
+            }
+
+            if (!(is_redis_server_on && is_redis_key_exists)) {
+                return false
+            } else {
+                return true
             }
         } catch (error) {
-            on_error(error)
+            redis_return_obj.message = "Exception"
+            redis_return_obj.data = error
+            
+            if (redis_return_obj.server_on) {
+                redis_client.end(redis_client.connection_id)
+            }
+
+            return false
         }
     }
 
-    set_key_json_value(redis_key, obj, on_success, on_error) {
+    get_value(redis_key, on_success, on_error) {
         try {
-            if (obj === null) {
-                on_error("'obj' can not be null")
-            }
+            if (this.create_redis_client(redis_key)) {
+                redis_client.get(redis_key, function (err, reply) {
+                    redis_return_obj.data = (!(reply === undefined) && (reply !== "")) ? reply : null
+                    redis_return_obj.message = (!(reply === undefined) && (reply !== "")) ? "Data was retrieved from Redis Cache Server" : "Result not found"
 
-            this.create_redis_client()
-
-            if (is_redis_server_on) {
-                redis_client.set(redis_key, JSON.stringify(obj), function (err, reply) {
-                    if (reply === "OK") {
-                        on_success(reply)
-                    }
-
-                    console.log("Redis 'set' operation message for '" + redis_key + "': " + reply)
+                    on_success(redis_return_obj)
+                    redis_client.quit()
                 })
+            } else {
+                on_error(redis_return_obj)
+
+                if (redis_return_obj.server_on) {
+                    redis_client.end(redis_client.connection_id)
+                }
             }
         } catch (error) {
-            on_error(error)
+            redis_return_obj.data = null
+            redis_return_obj.message = error
+
+            on_error(redis_return_obj)
+
+            if (redis_return_obj.server_on) {
+                redis_client.end(redis_client.connection_id)
+            }
+        }
+    }
+
+    set_value(redis_key, obj, on_success, on_error) {
+        try {
+            if (this.create_redis_client(redis_key, true)) {
+                var str_obj = JSON.stringify(obj)
+                redis_client.set(redis_key, str_obj, function (err, reply) {
+                    if (reply === "OK") {
+                        redis_return_obj.message = "Data was successfully set to key '" + redis_key + "' as the server sent '" + reply + "'"
+                        redis_return_obj.data = str_obj
+
+                        on_success(redis_return_obj)
+                        redis_client.quit()
+                    } else {
+                        throw err
+                    }
+                })
+            } else {
+                on_error(redis_return_obj)
+
+                if (redis_return_obj.server_on) {
+                    redis_client.end(redis_client.connection_id)
+                }
+            }
+        } catch (error) {
+            redis_return_obj.data = null
+            redis_return_obj.message = error
+
+            on_error(redis_return_obj)
+
+            if (redis_return_obj.server_on) {
+                redis_client.end(redis_client.connection_id)
+            }
         }
     }
 }
--- stock_listing/routes/index.js
+++ stock_listing/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const stock_dao = require('../data/dal_stock');
 const HttpStatus = require('http-status-codes');
 const redis = require('../data/redis_manager')
-
 const redis_key_stock_code_list = "sosi_ms0002_stock_listing.get_stock_code_list"
 
 /* GET */
@@ -35,28 +34,35 @@ router.get('/stock_code_list', function (req, res, next) {
   var redis_mngr = new redis()
 
   //Trying to get data from Redis
-  redis_mngr.get_key_json_value(redis_key_stock_code_list, function (s_data) {
-    if (s_data !== null) {
-      console.log("Data was retrieved from Redis...")
-      res.status(HttpStatus.OK).send(JSON.parse(s_data));
+  redis_mngr.get_value(redis_key_stock_code_list, function (obj) {    
+    if (obj.data !== null) {
+      console.info(obj.message)
+      res.status(HttpStatus.OK).send(JSON.parse(obj.data));
     } else {
-      //Going to main db to retrieve the data. Then, save this data to Redis
+      //Going to main db to retrieve the data if some error occurr when getting from Redis
       new stock_dao()
         .get_stock_code_list(function (data) {
-          console.log("Data was retrieved from main db...")
-          //Adding data to Redis
-          redis_mngr.set_key_json_value(redis_key_stock_code_list, data, function (s_data) {
-            res.status(HttpStatus.OK).send(data);
-          }, function (e_data) {
-            console.error(e_data)
-          })
+          console.log("Data was retrieved from main db")
+          res.status(HttpStatus.OK).send(data);
         }, function (data) {
           res.status(HttpStatus.METHOD_FAILURE).send(data);
         });
     }
-  }, function (e_data) {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(e_data);
+  }, function (obj) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(obj);
   })
+});
+
+/* UPDATE STOCK CODE LIST ON CACHE SERVER */
+router.put('/stock_code_list', function (req, res, next) {
+  var redis_mngr = new redis()
+  var stock = new stock_dao()
+
+  stock.get_stock_code_list(function (data) {
+    redis_mngr.set_value(redis_key_stock_code_list, data, function (obj) {
+      res.status(HttpStatus.OK).send(obj)
+    }, function (e_data) {})
+  }, function (data) {})
 });
 
 /* POST */
